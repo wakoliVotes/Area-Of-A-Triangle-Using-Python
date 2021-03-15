2577201bb7a3e2d11838a698ba3ceffb24094e36
unknown <votesdw@yahoo.com>
2021-03-15 20:42:30 +0300

    Third commit Area of a Triangle

---
+++ AreaofaTriangle.py
@@ -0,0 +1,21 @@
+#This program is aimed at calculating the area of a triagle
+#A triangle has three (3) sides, which will be used in this case
+print("                  ")
+print("                /|")
+print("               / |")
+print("              /  |")
+print("             /   |")
+print(" Hypotenuse /    |")
+print("           /     | Opposite/Height")
+print("          /      |")
+print("         /       |")
+print("        /        |")
+print("       /_________|")
+print("         Adjacent ")
+
+
+
+Adjacent = input("Enter the Adjacent side:: ")
+Opposite = input("Enter the Opposite side:: ")
+AreaofTriangle = 1/2 * float(Adjacent) * float(Opposite)
+print("Area of the Triangle is::", AreaofTriangle)
