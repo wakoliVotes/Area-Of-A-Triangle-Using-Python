1b891bfa522e73c2e79f628e958ad28314d1b06d
Danny-Votez <77758884+danny-votez@users.noreply.github.com>
2021-09-26 01:46:06 +0300

    Update AreaofaTriangle.py

--- AreaofaTriangle.py
+++ AreaofaTriangle.py
@@ -19,3 +19,7 @@ Adjacent = input("Enter the Adjacent side:: ")
 Opposite = input("Enter the Opposite side:: ")
 AreaofTriangle = 1/2 * float(Adjacent) * float(Opposite)
 print("Area of the Triangle is::", AreaofTriangle)
+
+
+# Adopting functions in completing the same program will have a different structure
+# In the next illustration, the aim is utilizing function definition on the Area of a Triangle scenario
